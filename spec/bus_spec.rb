1029318d4278b07992c6b2f5d2c14a2f8123633d
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-07 13:35:22 -0700

    Update Iteration 1

--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -3,25 +3,25 @@ require './lib/bus'
 
 describe Bus do
   context 'Attributes' do
-    xit 'is a bus' do
+    it 'is a bus' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus).to be_an_instance_of(Bus)
     end
 
-    xit 'has a name' do
+    it 'has a name' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.name).to eq("Mikes Awesome Bus")
     end
 
-    xit 'has a capacity' do
+    it 'has a capacity' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.capacity).to eq(4)
     end
 
-    xit 'has no passengers by default' do
+    it 'has no passengers by default' do
       bus = Bus.new('Mikes Awesome Bus', 4)
 
       expect(bus.passengers).to eq([])

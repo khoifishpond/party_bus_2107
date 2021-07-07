675617e2c08b6653669a6c4918558842fbfc0475
Khoi Nguyen <khoinguyen311@gmail.com>
2021-07-07 13:45:18 -0700

    Add Capacity to spec

--- spec/bus_spec.rb
+++ spec/bus_spec.rb
@@ -48,4 +48,15 @@ describe Bus do
       expect(bus.yell_at_passengers).to eq(['MIKE', 'MEGAN', 'TIM'])
     end
   end
+
+  context 'Capacity' do
+    it 'counts number of passengers' do
+      bus = Bus.new('Mikes Awesome Bus', 4)
+      bus.add_passenger('Mike')
+      bus.add_passenger('Megan')
+      bus.add_passenger('Tim')
+
+      expect(bus.number_of_passengers).to eq(3)
+    end
+  end
 end
